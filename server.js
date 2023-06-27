ba2668d6c3d460c7c24fb9467132b64fc86c6a78
Brent <brentmueller32@gmail.com>
2023-06-27 15:56:18 -0700

    found errors

--- server.js
+++ server.js
@@ -1,26 +1,40 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const methodOverride = require('method-override');
+const indexRouter = require('./routes/index');
+const skillsRouter = require('./routes/skills');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+
+app.use(function(req, res, next){
+  console.log('middleware function is running');
+
+  next()
+})
+
+
+app.use(methodOverride('_method'))
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// sets up the router object on the app
+// we can put each router, an adress 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
+// / says the router (userRouter) and the routes inside of it 
+// are prepended with /skills, starts with /skills and then whatever is in the userRouter
+app.use('/skills', skillsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
